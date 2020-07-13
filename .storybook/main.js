6bb4b823c1e585f53adda835aed64f0d16a951b7
James Shakespeare <j@jshakespeare.com>
2020-07-13 14:57:01 -0400

    webpack scss config and deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require( 'path' );
+
 module.exports = {
 	stories: [
 		'../src/**/*.stories.mdx',
@@ -5,4 +7,30 @@ module.exports = {
 	addons: [
 		'@storybook/addon-docs',
 	],
+	webpackFinal: async config => {
+		// Loads .scss files using our sassconfig
+		config.module.rules.push( {
+			test: /\.s?css$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						modules: {
+							localIdentName: '[name]__[local]__[hash:base64:5]',
+						},
+						sourceMap: true,
+						importLoaders: 1,
+						localsConvention: 'dashes',
+					},
+				},
+				'sass-loader',
+			],
+			include: [
+				path.resolve( __dirname, '../src' ),
+			],
+		} );
+
+		return config;
+	}
 };

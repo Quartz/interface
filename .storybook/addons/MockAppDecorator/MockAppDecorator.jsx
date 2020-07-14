435c4e34a8a78430dbf15c5d3040e6384a9ba801
James Shakespeare <j@jshakespeare.com>
2020-07-14 14:18:39 -0400

    add decorator component comment

--- .storybook/addons/MockAppDecorator/MockAppDecorator.jsx
+++ .storybook/addons/MockAppDecorator/MockAppDecorator.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '!style-loader!css-loader!sass-loader!./MockAppDecorator.scss';
 
+/*
+	A decorator component that provides baseline conditions required to
+	display library components. All story components are automatically
+	wrapped (see .storybook/preview.js)
+
+	https://storybook.js.org/docs/addons/introduction/#1-decorators
+*/
 const MockAppDecorator = ( { children } ) => (
 	<main className="fonts-loaded">{children}</main>
 );

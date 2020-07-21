c5517edd6fe8ba8fa445539a9de2e0c15db93676
James Shakespeare <j@jshakespeare.com>
2020-07-21 15:16:39 -0400

    document checkbox props, stub out radio button docs

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -37,16 +37,55 @@ const Checkbox = ( {
 );
 
 Checkbox.propTypes = {
+	/**
+	 * Forwarded to the input element as `aria-described-by`.
+	 */
 	ariaDescribedBy: PropTypes.string,
+	/**
+	 * Forwarded to the input element as `aria-label`.
+	 */
 	ariaLabel: PropTypes.string,
+	/**
+	 * Forwarded to the input element as `aria-labelled-by`.
+	 */
 	ariaLabelledBy: PropTypes.string,
+	/**
+	 * Toggle for the component’s checked state. Forwarded to the input
+	 * element.
+	 */
 	checked: PropTypes.bool,
+	/**
+	 * Child nodes to be rendered as the label for the checkbox.
+	 * Label/input happens automatically.
+	 */
 	children: PropTypes.node,
+	/**
+	 * Whether the checkbox is interactive. Forwarded to the input
+	 * element.
+	 */
 	disabled: PropTypes.bool.isRequired,
+	/**
+	 * HTML ID of the component. Forwarded to the input element.
+	 */
 	id: PropTypes.string,
+	/**
+	 * Indicates a validation error that must be corrected by the user.
+	 * Forwarded to the input element as `aria-invalid`.
+	 */
 	invalid: PropTypes.bool,
+	/**
+	 * Function to run when the checkbox is toggled. Forwarded to the
+	 * input element.
+	 */
 	onChange: PropTypes.func.isRequired,
+	/**
+	 * Indicates that the checkbox must be checked by the user in order
+	 * for the parent form to validate. Forwarded to the input element.
+	 */
 	required: PropTypes.bool.isRequired,
+	/**
+	 * Visual size of the checkbox. Does not affect label size.
+	 */
 	size: PropTypes.oneOf( [ 'small', 'large' ] ),
 };
 
--- src/components/Checkbox/Checkbox.story.jsx
+++ src/components/Checkbox/Checkbox.story.jsx
@@ -11,3 +11,6 @@ export default {
 };
 
 export const Default = () => <Checkbox>Check me out!</Checkbox>;
+export const Large = () => <Checkbox size="large">Check me out!</Checkbox>;
+export const Invalid = () => <Checkbox invalid={true}>You must check this box to continue</Checkbox>;
+export const Disabled = () => <Checkbox disabled={true}>Can't check this</Checkbox>;

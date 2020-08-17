f94d2e473281f7b9cfca6178d88f9015800f4a26
Brendan Hamill <bhamill@qz.com>
2020-08-17 13:46:45 -0400

    Fleshes out story

--- src/components/Header/Header.story.jsx
+++ src/components/Header/Header.story.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import Header from './Header';
+import docs from './Header.docs.mdx';
 
-export const Default = () => <Header title="Good morning, USA" intro="I’ve got a feeling that it’s gonna be a wonderful day" />;
+export const Default = () => (
+	<div>
+		<Header title="Good morning, USA" intro="I’ve got a feeling that it’s gonna be a wonderful day" />
+		<div>
+			<ul>
+				<li>Contributor 1</li>
+				<li>Contributor 2</li>
+				<li>Contributor 3</li>
+			</ul>
+		</div>
+	</div>
+);
 
-export default { title: 'Header' };
+export default {
+	conponent: Header,
+	parameters: {
+		docs: { page: docs },
+	},
+	title: 'Header',
+};

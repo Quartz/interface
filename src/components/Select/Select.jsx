ea866d4e2a1feb7e641257b65b6648e92bd95f80
Jessica Wray <jwray@qz.com>
2020-08-21 08:27:51 -0700

    OK I understand Storybook now

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -7,11 +7,11 @@ import ExpandArrowDown from './expand-arrow-down.svg';
 const cx = classnames.bind( styles );
 
 const Select = ( {
-	subtext,
 	disabled,
-	onChange,
+	hint,
 	invalid,
 	label,
+	onChange,
 	options,
 	placeholder,
 	required,
@@ -48,35 +48,54 @@ const Select = ( {
 						</option>
 					) )}
 				</select>
-				<ExpandArrowDown className={cx( 'down-arrow', { invalid } )} aria-hidden="true" />
+				<ExpandArrowDown
+					className={cx( 'down-arrow', { invalid } )}
+					aria-hidden="true"
+				/>
 			</div>
 		</label>
 		{
-			subtext &&
-			<span className={cx( 'subtext', { invalid } )}>{subtext}</span>
+			hint &&
+			<span className={cx( 'subtext', { invalid } )}>
+				{hint}
+			</span>
 		}
 	</div>
 );
 
 Select.propTypes = {
 	/**
-	 * If true, the element is not interactive. False by default.
+	 * If true, the element is not interactive.
 	 */
 	disabled: PropTypes.bool.isRequired,
+
+	/**
+	 * Instructive/helpful text to be displayed below the select.
+	 * e.g.
+	 *   "Your password must be 6 characters or more"
+	 *   "Unfortunately, we cannot accept JCB cards at this time"
+	 *
+	 * Note: if the element's invalid prop is true, this text will display red.
+	 */
+	hint: PropTypes.string,
+
 	/**
 	 * Whether the element is in an invalid state (or part of a group of
 	 * elements that are).
 	 */
-	invalid: PropTypes.bool,
+	invalid: PropTypes.bool.isRequired,
+
 	/**
-	 * A label for the whole element.
+	 * A label for the whole element, describing the category of options. For example: for a dropdown prompting the user to choose their state of residence, the label could be US State.
 	 */
-	label: PropTypes.string,
+	label: PropTypes.string.isRequired,
 	/**
 	 * A function that will called on change. It's passed to the native select's onChange handler.
 	 */
 	onChange: PropTypes.func.isRequired,
-	/** The options to display in the dropdown. Each has a label, which will display to the user, and a value, which can be different than the label. */
+	/**
+	 * The options to display in the dropdown. Each has a label, which will display to the user, and a value, which can be different than the label. For example: in a dropdown fo the user to choose their state of residence, the label might be the state name ("New Hampshire") and the value could be what's saved in the database, like a postal code ("NH")
+	 */
 	options: PropTypes.arrayOf( PropTypes.shape( {
 		label: PropTypes.string.isRequired,
 		value: PropTypes.oneOfType( [
@@ -86,21 +105,15 @@ Select.propTypes = {
 	} ) ).isRequired,
 
 	/**
-	 * A non-selectable option. Use this as an example or instruction.
-	 * ("Pick an option")
+	 * A non-selectable option, which will be the initial value if none is provided. Use this as an example or instruction.
+	 * ("Pick a state")
 	 */
 	placeholder: PropTypes.string,
 
 	/**
 	 * Whether a selection is required.
 	 */
-	required: PropTypes.bool,
-
-	/**
-	 * Text to be displayed below the select. If the element's invalid prop is true,
-	 * this will be styled as an error.
-	 */
-	subtext: PropTypes.string,
+	required: PropTypes.bool.isRequired,
 
 	/**
 	 * The value of the select element when used as a [controlled component](https://reactjs.org/docs/forms.html#controlled-components)

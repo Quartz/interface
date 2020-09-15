0f15ce7919ac42b3b64e18db57f96b8d3db86f6a
chriszarate <chris@zarate.org>
2020-09-15 17:56:43 -0400

    Update PageHeader with new font size and tagline

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tagline from '../Tagline/Tagline';
 import styles from './PageHeader.scss';
 
-const PageHeader = ( { border, children, intro, title } ) => (
+const PageHeader = ( { border, children, intro, tagline, title } ) => (
 	<div className={`${styles.container} ${border ? styles.border : ''}`}>
 		<div className={styles.contents}>
 			<h1 className={styles.title}>{title}</h1>
-			<p className={styles.intro}>{intro}</p>
+			{
+				tagline &&
+					<div className={styles.tagline}>
+						<Tagline>{tagline}</Tagline>
+					</div>
+			}
+			{
+				intro &&
+					<p className={styles.intro}>{intro}</p>
+			}
 		</div>
 		{children}
 	</div>
@@ -17,14 +27,22 @@ PageHeader.propTypes = {
 	 * Boolean to determine whether or not the component renders with a bottom border.
 	 */
 	border: PropTypes.bool.isRequired,
+
 	/**
 	 * Child node to be rendered as the inner HTML of the component.
 	 */
 	children: PropTypes.node,
+
 	/**
-	 * Smaller-print text used for the page subheader (renders as `<p>` tag).
+	 * Smaller-print text used for the page subheader (renders in `<p>` tag).
 	 */
-	intro: PropTypes.string,
+	intro: PropTypes.node,
+
+	/**
+	 * Very small-print text used directly below the title (e.g., as a dateline).
+	 */
+	tagline: PropTypes.node,
+
 	/**
 	 * Required text for the h1 tag.
 	 */

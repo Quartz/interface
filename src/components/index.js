4c78cc432aabbbc0e6c112156b085e41dc40dfdf
Chris Zarate <chris@zarate.org>
2020-09-29 16:12:32 -0400

    Consolidate EmojiList and implement render prop for CSS (#55)

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ export { default as Blockquote } from './Blockquote/Blockquote';
 export { default as CalloutCard } from './CalloutCard/CalloutCard';
 export { default as Checkbox } from './Checkbox/Checkbox';
 export { default as ColorScheme, schemes } from './ColorScheme/ColorScheme';
-export { EmojiList, StructuredEmojiList } from './EmojiList/EmojiList';
+export { default as EmojiList } from './EmojiList/EmojiList';
 export { default as Hed } from './Hed/Hed';
 export { default as Image } from './Image/Image';
 export { default as Kicker } from './Kicker/Kicker';

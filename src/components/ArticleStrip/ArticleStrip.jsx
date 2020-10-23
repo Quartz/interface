255545acb128b6f5f84e3c272364fc42516254db
James Shakespeare <jshakespeare@qz.com>
2020-10-23 11:47:35 -0400

    Adds ArticleStrip component (#63)
    * add new component script
    
    * use npm run new for new components
    
    * stub out articlestrip
    
    * add some basic styling and tighten up props
    
    * use ResponsiveImage for thumbnail
    
    * use TextGroup component for article strip text
    
    * large size article strip
    
    * small name tweak
    
    * prop docblocks, more examples
    
    * more docs, pass in dateGmt prop
    
    * upgrade to latest js-utils and use timestamp helper
    
    * keep sizes simple, stupid
    
    * remove test

---
+++ src/components/ArticleStrip/ArticleStrip.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ArticleStrip.scss';
+import { ResponsiveImage, TextGroup } from '..';
+import { stylizedTimestamp } from '@quartz/js-utils';
+
+const responsiveImagePropsMapping = {
+	small: {
+		fallbackWidth: 80,
+		fallbackHeight: 80,
+		sizes: '80px',
+		widthRange: [ 80, 80 ],
+	},
+	'extra-large': {
+		fallbackWidth: 220,
+		fallbackHeight: 220,
+		sizes: `
+			(min-width: 1200px) 220px,
+			(min-width: 768px) 175px,
+			80px
+		`,
+		widthRange: [ 80, 220 ],
+	},
+};
+
+function ArticleStrip ( {
+	dateGmt,
+	edition,
+	kicker,
+	size,
+	thumbnailUrl,
+	title,
+} ) {
+	return (
+		<div className={`${styles.container} ${styles[ size ]}`}>
+			<div className={`${styles.thumbnailContainer} ${styles[ size ]}`}>
+				<ResponsiveImage
+					alt=""
+					src={thumbnailUrl}
+					{...responsiveImagePropsMapping[ size ]}
+				/>
+			</div>
+			<div>
+				<TextGroup
+					isArticle={true}
+					kicker={kicker}
+					size={size}
+					title={title}
+					tagline={`${stylizedTimestamp( dateGmt )} • ${edition}`}
+				/>
+			</div>
+		</div>
+	);
+}
+
+ArticleStrip.propTypes = {
+	/**
+	 * A string-based representation of the article publish datetime at
+	 * the Greenwich Mean Time (GMT-00:00:00). E.g.,
+	 * `'2012-09-04T17:02:10'`.
+	*/
+	dateGmt: PropTypes.string.isRequired,
+
+	/**
+	 * The Quartz edition to which the article belongs.
+	 */
+	edition: PropTypes.oneOf( [
+		'Quartz',
+		'Quartz Africa',
+		'Quartz India',
+		'Quartz at Work',
+	] ).isRequired,
+
+	/**
+	 * A short phrase that accompanies the hed. See [Kicker](/?path=/docs/kicker--default-story).
+	 */
+	kicker: PropTypes.string,
+
+	/**
+	 * The size of the component. Adjusts the thumbnail dimensions and
+	 * [TextGroup](/?path=/docs/textgroup--default-story) size used.
+	 */
+	size: PropTypes.oneOf( [ 'small', 'extra-large' ] ),
+
+	/**
+	 * URL of the thumbnail image from our WordPress media library.
+	 */
+	thumbnailUrl: PropTypes.string.isRequired,
+
+	/**
+	 * The article headline. See [Hed](/?path=/docs/hed--default-story)
+	 */
+	title: PropTypes.string.isRequired,
+};
+
+ArticleStrip.defaultProps = {
+	edition: 'Quartz',
+	size: 'small',
+};
+
+export default ArticleStrip;

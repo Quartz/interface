66668af941507e839cc3735ad138604739512cb8
chriszarate <chris@zarate.org>
2020-12-04 15:21:24 -0500

    Update stories

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -4,14 +4,14 @@ import classnames from 'classnames/bind';
 
 const cx = classnames.bind( styles );
 
-export default function TextInput ( props: {
+export function TextInput ( props: {
 	/**
 	 * Used with aria-describedby attribute to indicate the ID of a linked description element.
 	 */
 	ariaDescribedBy?: string,
 
 	/**
-	 * Sets for the browser what values the browser should suggest to fill out the field.
+	 * Informs the browser what values should be suggested when filling out the field.
 	 */
 	autoComplete:
 		'country' |
@@ -26,38 +26,37 @@ export default function TextInput ( props: {
 		'url',
 
 	/**
-	 * Sets whether the input is prevented from changing values or not. Distinct from read-only in that users cannot
-	 * click or select in the field.
+	 * For uncontrolled components, sets the initial value which can be overridden by the user.
+	 */
+	defaultValue?: string,
+
+	/**
+	 * Sets whether the field is prevented from changing values or not. Distinct from read-only in that users cannot click or select in the field.
 	 */
 	disabled?: boolean,
 
 	/**
-	 * Descriptive content displayed under the element, usually to provide context or instructions to the user (e.g. 'Password must be at least 6 characters' )
+	 * Descriptive content displayed under the field, usually to provide context or instructions to the user (e.g., "Password must be at least 6 characters").
 	 */
 	hint?: React.ReactNode,
 
 	/**
-	 * HTML ID of the component. Forwarded to the input/textarea element.
+	 * HTML ID. Forwarded to the element.
 	 */
 	id: string,
 
 	/**
-	 * Forward a ref to the input element. Use this prop to create an uncontrolled component.
+	 * Forward a ref to the element. Use this prop to create an uncontrolled component.
 	 */
 	inputRef?: React.Ref<HTMLInputElement>,
 
 	/**
-	 * Alert the user that validation has failed. This may be useful where HTML5 validation is not sufficient (for example, a field that is validated server side)
+	 * Alert the user that validation has failed. This may be useful where HTML5 validation is not sufficient (for example, a field that is validated server-side).
 	*/
 	invalid?: boolean,
 
 	/**
-	 * Boolean that determines whether component is a `textarea` or `input` tag.
-	 */
-	isMultiline?: boolean,
-
-	/**
-	 * Semantic description of the input or textarea purpose to be displayed above the element (e.g. 'Email' for an email input).
+	 * Semantic description of the field's purpose to be displayed above the field (e.g., "Email" for an email input).
 	 */
 	label?: string,
 
@@ -67,29 +66,29 @@ export default function TextInput ( props: {
 	maxLength?: number,
 
 	/**
-	 * Event handler fired when focus is removed from the tag.
+	 * Event handler fired when focus is removed from the element.
 	 */
-	onBlur?: ( event: InputEvent ) => void,
+	onBlur?: ( event: React.FocusEvent ) => void,
 
 	/**
 	 * Event handler fired when user input changes.
 	 */
-	onChange?: ( event: InputEvent ) => void,
+	onChange?: ( event: React.ChangeEvent ) => void,
 
 	/**
-	 * Event handler for when a user clicks onto the element.
+	 * Event handler for when a user clicks on the element.
 	 */
-	onClick?: ( event: InputEvent ) => void,
+	onClick?: ( event: React.MouseEvent ) => void,
 
 	/**
 	 * Event handler fired when a user focuses on the element, including tab-focus.
 	 */
-	onFocus?: ( event: InputEvent ) => void,
+	onFocus?: ( event: React.FocusEvent ) => void,
 
 	/**
-	 * Event handler fired when input is found invalid (see `pattern` / `type`).
+	 * Event handler fired when input is found invalid.
 	 */
-	onInvalid?: ( event: InputEvent ) => void,
+	onInvalid?: ( event: React.FormEvent ) => void,
 
 	/**
 	 * A regex validation that the input must match for input to be valid (for example, a custom email validator).
@@ -97,13 +96,12 @@ export default function TextInput ( props: {
 	pattern?: string,
 
 	/**
-	 * Sets 'ghost' text that appears within the input or textarea field when no value exists. For example, if the label
-	 * suggests the input is for email, set placeholder to an example email ("email@qz.com").
+	 * Sets "ghost" text that appears within the field when no value exists. For example, if the label suggests the input is for email, set placeholder to "email@qz.com".
 	 */
 	placeholder?: string,
 
 	/**
-	 * Determines whether value is editable or not. Users can still click/select/highlight in the field.
+	 * Determines whether field value is editable or not. Users can still click/select/highlight in the field.
 	 */
 	readOnly?: boolean,
 
@@ -113,9 +111,7 @@ export default function TextInput ( props: {
 	required?: boolean,
 
 	/**
-	 * For input element only - determines type of form control, defaulting to text. Depending on the input type,
-	 * different browser validations may apply (e.g. 'password' type will automatically obscure text input).
-	 * Overview of types (MDN): https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+	 * Determines type of form control. Depending on the input type, different browser validations may apply (e.g., "password" type will automatically obscure text input). Overview of types (MDN): https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input.
 	 */
 	type:
 		'text' |
@@ -129,7 +125,7 @@ export default function TextInput ( props: {
 		'datetime',
 
 	/**
-	 *  If present, the value of the form field. Use this prop to create a controlled component.
+	 *  If present, the value of the field. Use this prop to create a controlled component.
 	 */
 	value?: string,
 } ) {
@@ -137,7 +133,6 @@ export default function TextInput ( props: {
 		autoComplete = 'on',
 		disabled = false,
 		invalid = false,
-		isMultiline = false,
 		readOnly = false,
 		required = false,
 	} = props;
@@ -151,50 +146,164 @@ export default function TextInput ( props: {
 					</span>
 			}
 
+			<input
+				aria-describedby={props.ariaDescribedBy}
+				aria-invalid={invalid}
+				autoComplete={autoComplete}
+				className={styles.input}
+				defaultValue={props.defaultValue}
+				disabled={disabled}
+				id={props.id}
+				maxLength={props.maxLength}
+				onBlur={props.onBlur}
+				onChange={props.onChange}
+				onClick={props.onClick}
+				onFocus={props.onFocus}
+				onInvalid={props.onInvalid}
+				pattern={props.pattern}
+				placeholder={props.placeholder}
+				readOnly={readOnly}
+				ref={props.inputRef}
+				required={required}
+				type={props.type}
+				value={props.value}
+			/>
+
 			{
-				isMultiline ?
-					<textarea
-						className={styles.textarea}
-						rows={6}
-						aria-describedby={props.ariaDescribedBy}
-						aria-invalid={invalid}
-						disabled={disabled}
-						id={props.id}
-						maxLength={props.maxLength}
-						placeholder={props.placeholder}
-						onBlur={props.onBlur}
-						onChange={props.onChange}
-						onClick={props.onClick}
-						onFocus={props.onFocus}
-						onInvalid={props.onInvalid}
-						readOnly={readOnly}
-						ref={props.inputRef}
-						required={required}
-						value={props.value}
-					/>
-					:
-					<input
-						className={styles.input}
-						type={props.type}
-						aria-describedby={props.ariaDescribedBy}
-						aria-invalid={invalid}
-						autoComplete={autoComplete}
-						disabled={disabled}
-						id={props.id}
-						maxLength={props.maxLength}
-						placeholder={props.placeholder}
-						readOnly={readOnly}
-						onBlur={props.onBlur}
-						onChange={props.onChange}
-						onClick={props.onClick}
-						onFocus={props.onFocus}
-						onInvalid={onInvalid}
-						pattern={props.pattern}
-						ref={props.inputRef}
-						required={required}
-						value={props.value}
-					/>
+				props.hint &&
+					<div className={cx( 'hint', { invalid } )}>{props.hint}</div>
 			}
+		</label>
+	);
+}
+
+export function TextMultilineInput ( props: {
+	/**
+	 * Used with aria-describedby attribute to indicate the ID of a linked description element.
+	 */
+	ariaDescribedBy?: string,
+
+	/**
+	 * For uncontrolled components, sets the initial value which can be overridden by the user.
+	 */
+	defaultValue?: string,
+
+	/**
+	 * Sets whether the field is prevented from changing values or not. Distinct from read-only in that users cannot click or select in the field.
+	 */
+	disabled?: boolean,
+
+	/**
+	 * Descriptive content displayed under the field, usually to provide context or instructions to the user.
+	 */
+	hint?: React.ReactNode,
+
+	/**
+	 * HTML ID. Forwarded to the element.
+	 */
+	id: string,
+
+	/**
+	 * Forward a ref to the element. Use this prop to create an uncontrolled component.
+	 */
+	inputRef?: React.Ref<HTMLTextAreaElement>,
+
+	/**
+	 * Alert the user that validation has failed. This may be useful where HTML5 validation is not sufficient (for example, a field that is validated server-side).
+	*/
+	invalid?: boolean,
+
+	/**
+	 * Semantic description of the field’s purpose to be displayed above the field.
+	 */
+	label?: string,
+
+	/**
+	 * Prevents string length beyond given value.
+	 */
+	maxLength?: number,
+
+	/**
+	 * Event handler fired when focus is removed from the element.
+	 */
+	onBlur?: ( event: React.FocusEvent ) => void,
+
+	/**
+	 * Event handler fired when user input changes.
+	 */
+	onChange?: ( event: React.ChangeEvent ) => void,
+
+	/**
+	 * Event handler for when a user clicks on the element.
+	 */
+	onClick?: ( event: React.MouseEvent ) => void,
+
+	/**
+	 * Event handler fired when a user focuses on the element, including tab-focus.
+	 */
+	onFocus?: ( event: React.FocusEvent ) => void,
+
+	/**
+	 * Event handler fired when input is found invalid.
+	 */
+	onInvalid?: ( event: React.FormEvent ) => void,
+
+	/**
+	 * Sets "ghost" text that appears within the element when no value exists.
+	 */
+	placeholder?: string,
+
+	/**
+	 * Determines whether the field value is editable or not. Users can still click/select/highlight in the field.
+	 */
+	readOnly?: boolean,
+
+	/**
+	 * If true, value must be present for the form to be submitted. Will add an asterisk to label if label is present.
+	 */
+	required?: boolean,
+
+	/**
+	 *  If present, the value of the field. Use this prop to create a controlled component.
+	 */
+	value?: string,
+} ) {
+	const {
+		disabled = false,
+		invalid = false,
+		readOnly = false,
+		required = false,
+	} = props;
+
+	return (
+		<label className={styles.container}>
+			{
+				props.label &&
+					<span className={cx( 'label', { required } )}>
+						{props.label}
+					</span>
+			}
+
+			<textarea
+				aria-describedby={props.ariaDescribedBy}
+				aria-invalid={invalid}
+				className={styles.textarea}
+				defaultValue={props.defaultValue}
+				disabled={disabled}
+				id={props.id}
+				maxLength={props.maxLength}
+				onBlur={props.onBlur}
+				onChange={props.onChange}
+				onClick={props.onClick}
+				onFocus={props.onFocus}
+				onInvalid={props.onInvalid}
+				placeholder={props.placeholder}
+				readOnly={readOnly}
+				ref={props.inputRef}
+				required={required}
+				rows={6}
+				value={props.value}
+			/>
 
 			{
 				props.hint &&
--- src/components/index.js
+++ src/components/index.js
@@ -19,4 +19,4 @@ export { default as Spinner } from './Spinner/Spinner';
 export { TabNav, TabNavItem } from './TabNav/TabNav';
 export { default as Tagline } from './Tagline/Tagline';
 export { default as TextGroup } from './TextGroup/TextGroup';
-export { default as TextInput } from './TextInput/TextInput';
+export { TextInput, TextMultilineInput } from './TextInput/TextInput';

b8a416efc2357ef139d9731237c6c4700c3a10df
Brendan Hamill <bhamill@qz.com>
2020-12-18 13:10:55 -0500

    Adds PlayIcon

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -19,32 +19,79 @@ const responsiveImagePropsMapping = {
 		`,
 		widthRange: [ 220, 666 ],
 	},
+	portrait: {
+		fallbackWidth: 285,
+		fallbackHeight: 400,
+		sizes: `
+			(min-width: 1200px) 382px,
+			285px,
+		`,
+		widthRange: [ 285, 382 ],
+	},
 };
 
 export default function FeatureCard( props: {
-	altText: string,
+	/**
+	 * Alternative text to describe the image for screen readers or in situations
+	 * where the image cannot be loaded. This prop is required but under certain
+	 * circumstances an empty string is preferred.
+	 */
+	alt: string,
+	/**
+	 * Optional maller text below the component title. As an example, if the component title
+	 * is "The rise of employee activists," a relevant description might be "Employee
+	 * activists are transforming the workplace—and making companies better in the process."
+	 */
 	description?: string,
+	/**
+	 * A short phrase that accompanies the Hed. See [Kicker](/?path=/docs/kicker--default-story).
+	 */
 	kicker?: string,
+	/**
+	 * URL of an image in the WordPress media library. Must support
+	 * resizing using the width (`w`), height (`h`) and crop (`crop`)
+	 * query parameters.
+	 */
 	src: string,
+	/**
+	 * The article headline, used in TextGroup as Hed. See [Hed](/?path=/docs/hed--default-story).
+	 */
 	title: string,
+	/**
+	 * Shows a typical "play" icon overlaid onto the component image. Use when the card represents
+	 * video content.
+	 */
 	showPlayIcon?: boolean,
-	size: 'small' | 'large',
+	/**
+	 * The expected size & dimensions of the image to be rendered. The 'small' size will optimize
+	 * the image for smaller contexts, such as content lists at mobile sizes. 'Large' and
+	 * 'portrait' sizes will optimize for contexts such as Guide cards and content lists at desktop
+	 * sizes. See [ResponsiveImage](/?path=/docs/responsiveimage--default-story).
+	 */
+	size: 'small' | 'large' | 'portrait',
 } ) {
 	return (
-		<div className={styles.container}>
-			<div className={`${styles.imageContainer} ${styles.size}`}>
+		<div>
+			<div className={`${styles.imageContainer} ${styles[ props.size ]}`}>
 				<ResponsiveImage
-					alt={props.altText}
+					alt={props.alt}
 					src={props.src}
 					{...responsiveImagePropsMapping[props.size]}
 				/>
+				{
+					props.showPlayIcon && (
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" className={styles.playIcon}>
+							<path d="M78.936 41.951L5.918 79.073a4 4 0 01-5.813-3.565V4.829a4 4 0 015.813-3.565l73.018 37.122a2 2 0 010 3.565z" fill="var(--color,#fff)" fill-rule="evenodd"/>
+						</svg>
+					)
+				}
 			</div>
 			<TextGroup
 				isArticle
 				kicker={props.kicker}
 				tagline={props.description}
 				title={props.title}
-				size={props.size}
+				size={props.size === 'portrait' ? 'small' : props.size}
 			/>
 		</div>
 	);

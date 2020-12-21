aa8f7d35ec418603d6bff48d21f4495a2ee48154
GitHub Action <action@github.com>
2020-12-21 18:03:31 +0000

    Add TypeScript types

--- dist/FeatureCard/FeatureCard.d.ts
+++ dist/FeatureCard/FeatureCard.d.ts
@@ -7,13 +7,6 @@ export default function FeatureCard(props: {
      */
     description?: string;
     /**
-     * The expected size & dimensions of the image to be rendered. The 'small' size will optimize
-     * the image for smaller contexts, such as content lists at mobile sizes. 'Large' and
-     * 'portrait' sizes will optimize for contexts such as Guide cards and content lists at desktop
-     * sizes. See [ResponsiveImage](/?path=/docs/responsiveimage--default-story).
-     */
-    imageSize: 'small' | 'large';
-    /**
      * Whether the card is for article content; passed through to TextGroup to
      * determine color and formatting. Defaults to `true`.
      */
@@ -34,17 +27,20 @@ export default function FeatureCard(props: {
      */
     thumbnailUrl: string;
     /**
-     * The size of the title text.
-     */
-    textSize: 'small' | 'medium' | 'large';
-    /**
-     * Primary text, used in TextGroup as Hed. See [Hed](/?path=/docs/hed--default-story).
-     */
-    title: string;
-    /**
      * Shows a typical "play" icon overlaid onto the component image. Use when the card represents
      * video content.
      */
     showPlayIcon?: boolean;
+    /**
+     * Determines the size of the title text and the expected size & dimensions of the image to be
+     * rendered. The 'small' size will optimize the image for smaller contexts, such as content lists
+     * at mobile sizes. 'Large' and 'portrait' sizes will optimize for contexts such as Guide cards
+     * and content lists at desktop sizes. See [ResponsiveImage](/?path=/docs/responsiveimage--default-story).
+     */
+    size: 'small' | 'medium' | 'large';
+    /**
+     * Primary text, used in TextGroup as Hed. See [Hed](/?path=/docs/hed--default-story).
+     */
+    title: string;
 }): JSX.Element;
 //# sourceMappingURL=FeatureCard.d.ts.map

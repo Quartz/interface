767e4f166980951a49ba808cc4c38f25eac137c3
GitHub Action <action@github.com>
2021-01-20 14:42:25 +0000

    Add TypeScript types

--- dist/ResponsiveImage/ResponsiveImage.d.ts
+++ dist/ResponsiveImage/ResponsiveImage.d.ts
@@ -1,29 +1,62 @@
-export default ResponsiveImage;
-declare function ResponsiveImage({ alt, amp, className, fallbackHeight, fallbackWidth, sizes, src, widthRange, }: {
-    alt: any;
-    amp: any;
-    className: any;
-    fallbackHeight: any;
-    fallbackWidth: any;
-    sizes: any;
-    src: any;
-    widthRange: any;
+/// <reference types="react" />
+export default function ResponsiveImage(props: {
+    /**
+     * Alternative text to describe the image for screen readers or in situations
+     * where the image cannot be loaded. This prop is required but under certain
+     * circumstances an empty string is preferred.
+     */
+    alt: string;
+    /**
+     * Whether to render the AMP version of the image. See
+     * https://amp.dev/documentation/components/amp-img/
+     */
+    amp?: boolean;
+    /**
+     * Passed verbatim to img or amp-img element as class attribute.
+     */
+    className?: string;
+    /**
+     * The rendered height of the image when CSS cannot be loaded or in very old
+     * browsers. With `fallbackWidth`, it sets the aspect ratio for the image.
+     * Therefore, it's critical to provide an accurate value. A good practice is
+     * to set this at the largest size at which this image can be rendered.
+     */
+    fallbackHeight: number;
+    /**
+     * The rendered width of the image when CSS cannot be loaded or in very old
+     * browsers. See `fallbackHeight`.
+     */
+    fallbackWidth: number;
+    /**
+     * Compression quality that will be passed to WordPress, 0-100. Defaults to 75.
+     */
+    quality?: number;
+    /**
+     * Desribes the width of the image slot to the browser. This can be
+     * as simple as a pixel-based value (for an image slot that will have
+     * a fixed width), or as complicated as a comma-separated list of
+     * media conditions mapped to image slot widths.
+     *
+     * By default, `sizes` will be derived from the `fallbackWidth`,
+     * which tells the browser the image slot will always have a fixed
+     * width.
+     *
+     * Forwarded to the HTML image element verbatim.
+     */
+    sizes?: string;
+    /**
+     * URL of an image in the WordPress media library. Must support
+     * resizing using the width (`w`), height (`h`) and crop (`crop`)
+     * query parameters.
+     */
+    src: string;
+    /**
+     * Array containing two values: the smallest and the largest widths
+     * at which the image is expected to be rendered, e.g. `[ 100, 200 ]`.
+     *
+     * For fixed-dimension images, pass the same value for the minimum
+     * and maximum values, e.g. `[ 100, 100 ]`.
+     */
+    widthRange: Array<number>;
 }): JSX.Element;
-declare namespace ResponsiveImage {
-    namespace propTypes {
-        const alt: PropTypes.Validator<string>;
-        const amp: PropTypes.Validator<boolean>;
-        const className: PropTypes.Requireable<string>;
-        const fallbackHeight: PropTypes.Validator<number>;
-        const fallbackWidth: PropTypes.Validator<number>;
-        const sizes: PropTypes.Requireable<string>;
-        const src: PropTypes.Validator<string>;
-        const widthRange: PropTypes.Validator<(number | null | undefined)[]>;
-    }
-    namespace defaultProps {
-        const amp_1: boolean;
-        export { amp_1 as amp };
-    }
-}
-import PropTypes from "prop-types";
 //# sourceMappingURL=ResponsiveImage.d.ts.map

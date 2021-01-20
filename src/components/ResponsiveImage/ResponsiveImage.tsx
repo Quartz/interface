946d3bba76b8aa7b6eb9ae54ba92a8a9b5a2fb52
Hal Lee <hal@lee.me>
2021-01-20 09:40:06 -0500

    Add quality parameter to ResponsiveImage (plus, TypeScript)

--- src/components/ResponsiveImage/ResponsiveImage.tsx
+++ src/components/ResponsiveImage/ResponsiveImage.tsx
@@ -1,18 +1,79 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Image } from '..';
 import { arrayFromRange, resizeWPImage } from '@quartz/js-utils';
 
-function ResponsiveImage( {
-	alt,
-	amp,
-	className,
-	fallbackHeight,
-	fallbackWidth,
-	sizes,
-	src,
-	widthRange,
+export default function ResponsiveImage( props: {
+	/**
+	 * Alternative text to describe the image for screen readers or in situations
+	 * where the image cannot be loaded. This prop is required but under certain
+	 * circumstances an empty string is preferred.
+	 */
+	alt: string;
+	/**
+	 * Whether to render the AMP version of the image. See
+	 * https://amp.dev/documentation/components/amp-img/
+	 */
+	amp?: boolean;
+	/**
+	 * Passed verbatim to img or amp-img element as class attribute.
+	 */
+	className?: string;
+	/**
+	 * The rendered height of the image when CSS cannot be loaded or in very old
+	 * browsers. With `fallbackWidth`, it sets the aspect ratio for the image.
+	 * Therefore, it's critical to provide an accurate value. A good practice is
+	 * to set this at the largest size at which this image can be rendered.
+	 */
+	fallbackHeight: number;
+	/**
+	 * The rendered width of the image when CSS cannot be loaded or in very old
+	 * browsers. See `fallbackHeight`.
+	 */
+	fallbackWidth: number;
+	/**
+	 * Compression quality that will be passed to WordPress, 0-100. Defaults to 75.
+	 */
+	quality?: number;
+	/**
+	 * Desribes the width of the image slot to the browser. This can be
+	 * as simple as a pixel-based value (for an image slot that will have
+	 * a fixed width), or as complicated as a comma-separated list of
+	 * media conditions mapped to image slot widths.
+	 *
+	 * By default, `sizes` will be derived from the `fallbackWidth`,
+	 * which tells the browser the image slot will always have a fixed
+	 * width.
+	 *
+	 * Forwarded to the HTML image element verbatim.
+	 */
+	sizes?: string;
+	/**
+	 * URL of an image in the WordPress media library. Must support
+	 * resizing using the width (`w`), height (`h`) and crop (`crop`)
+	 * query parameters.
+	 */
+	src: string;
+	/**
+	 * Array containing two values: the smallest and the largest widths
+	 * at which the image is expected to be rendered, e.g. `[ 100, 200 ]`.
+	 *
+	 * For fixed-dimension images, pass the same value for the minimum
+	 * and maximum values, e.g. `[ 100, 100 ]`.
+	 */
+	widthRange: Array<number>;
 } ) {
+	const {
+		alt,
+		amp = false,
+		className,
+		fallbackHeight,
+		fallbackWidth,
+		quality = 75,
+		sizes,
+		src,
+		widthRange,
+	} = props;
+
 	const [ minWidth, maxWidth ] = widthRange;
 	// Create an array of src widths based on the provided range
 	// Double the largest width to account for higher pixel density displays
@@ -24,7 +85,7 @@ function ResponsiveImage( {
 
 	// Map source widths to image URLs
 	const srcSet = srcWidths
-		.map( width => `${resizeWPImage( src, width, aspectRatio * width, true )} ${width}w` )
+		.map( width => `${resizeWPImage( src, width, aspectRatio * width, true, quality )} ${width}w` )
 		.join();
 
 	// If no `sizes` prop was specified, assume the image has a fixed width
@@ -43,73 +104,3 @@ function ResponsiveImage( {
 		/>
 	);
 }
-
-ResponsiveImage.propTypes = {
-	/**
-	 * Alternative text to describe the image for screen readers or in situations
-	 * where the image cannot be loaded. This prop is required but under certain
-	 * circumstances an empty string is preferred.
-	 */
-	alt: PropTypes.string.isRequired,
-
-	/**
-	 * Whether to render the AMP version of the image. See
-	 * https://amp.dev/documentation/components/amp-img/
-	 */
-	amp: PropTypes.bool.isRequired,
-
-	/**
-	 * Passed verbatim to img or amp-img element as class attribute.
-	 */
-	className: PropTypes.string,
-
-	/**
-	 * The rendered height of the image when CSS cannot be loaded or in very old
-	 * browsers. With `fallbackWidth`, it sets the aspect ratio for the image.
-	 * Therefore, it's critical to provide an accurate value. A good practice is
-	 * to set this at the largest size at which this image can be rendered.
-	 */
-	fallbackHeight: PropTypes.number.isRequired,
-
-	/**
-	 * The rendered width of the image when CSS cannot be loaded or in very old
-	 * browsers. See `fallbackHeight`.
-	 */
-	fallbackWidth: PropTypes.number.isRequired,
-
-	/**
-	 * Desribes the width of the image slot to the browser. This can be
-	 * as simple as a pixel-based value (for an image slot that will have
-	 * a fixed width), or as complicated as a comma-separated list of
-	 * media conditions mapped to image slot widths.
-	 *
-	 * By default, `sizes` will be derived from the `fallbackWidth`,
-	 * which tells the browser the image slot will always have a fixed
-	 * width.
-	 *
-	 * Forwarded to the HTML image element verbatim.
-	 */
-	sizes: PropTypes.string,
-
-	/**
-	 * URL of an image in the WordPress media library. Must support
-	 * resizing using the width (`w`), height (`h`) and crop (`crop`)
-	 * query parameters.
-	 */
-	src: PropTypes.string.isRequired,
-
-	/**
-	 * Array containing two values: the smallest and the largest widths
-	 * at which the image is expected to be rendered, e.g. `[ 100, 200 ]`.
-	 *
-	 * For fixed-dimension images, pass the same value for the minimum
-	 * and maximum values, e.g. `[ 100, 100 ]`.
-	 */
-	widthRange: PropTypes.arrayOf( PropTypes.number ).isRequired,
-};
-
-ResponsiveImage.defaultProps = {
-	amp: false,
-};
-
-export default ResponsiveImage;

a23f708523620a5b116e4baa0123fdfb15387aa5
Brendan Hamill <brenhamill@gmail.com>
2021-01-27 17:33:09 -0500

    Adds emails to ArticleStrip props + swap date/edition (#98)
    * Adds emails to ArticleStrip props + swap date/edition
    
    * Add TypeScript types
    
    Co-authored-by: Brendan Hamill <bhamill@qz.com>
    Co-authored-by: GitHub Action <action@github.com>

--- dist/Spinner/Spinner.d.ts
+++ dist/Spinner/Spinner.d.ts
@@ -1,3 +1,9 @@
+/// <reference types="react" />
+declare const Spinner: (props: {
+    /**
+     * Whether to display a timeout message to the user after a number of seconds have elapsed.
+     */
+    timeout?: boolean | undefined;
+}) => JSX.Element;
 export default Spinner;
-declare function Spinner(): JSX.Element;
 //# sourceMappingURL=Spinner.d.ts.map

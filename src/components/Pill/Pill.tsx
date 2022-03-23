38f26b79477c29c2e6020537ed218a555416799a
Sez <hey.sez@pm.me>
2022-03-22 17:58:38 -0700

    add alt type styles

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -10,11 +10,12 @@ type PillProps = {
    * Icon to display before the children.
    */
   icon?: string;
+  type?: 'default' | 'alt';
 };
 
-export default function Pill( { children, icon }: PillProps ) {
+export default function Pill( { children, icon, type = 'default' }: PillProps ) {
 	return (
-		<div className={styles.container}>
+		<div className={`${styles.container} ${type === 'alt' ? styles['alt-type'] : ''}`} >
 			{icon && <span className={styles.icon} aria-hidden="true">
 				{icon}
 			</span>}
